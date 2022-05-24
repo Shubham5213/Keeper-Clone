faf13a7092efcc84ecb25f361f2f0306b7755720
Shubham Kumar <66840922+Shubham5213@users.noreply.github.com>
2022-05-24 18:39:00 +0530

    keep notes app frontEnd-React

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CopyrightIcon from '@mui/icons-material/Copyright';
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>&copy; Saksena Inc. {year}</p>
+    </footer>
+  );
+}
+
+export default Footer;
